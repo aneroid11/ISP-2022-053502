704367502ccc511abeae4c8cb920fcb9a8043934
aneroid11 <a-e-v2003@yandex.by>
2022-02-21 21:19:35 +0300

    splitting text into sentences and counting median words amount

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,11 +1,3 @@
-"""
-На вход поступают текстовые данные. Необходимо посчитать и вывести:
-- сколько раз повторяется каждое слово в указанном тексте
-- среднее количество слов в предложении
-- медианное количество слов в предложении
-- top-K самых часто повторяющихся буквенных N-грам (K и N имеют значения по-умолчанию 10 и 4, но должна быть возможность
-задавать их с клавиатуры).
-"""
 import re
 
 N = 4
@@ -32,21 +24,53 @@ def count_every_word(text):
     return words_amounts
 
 
+def valid_sentence(inp_str: str):
+    return inp_str and not inp_str.isspace()
+
+
 def split_into_sentences(text):
     sentences = re.split("[!.?]", text)
-    sentences = list(filter(None, sentences))
+    sentences = list(filter(valid_sentence, sentences))
     return sentences
 
 
 def average_words_in_sentence(text):
     sentences = split_into_sentences(text)
 
+    print("\nSENTENCES")
+    for curr_sent in sentences:
+        print(curr_sent)
+    print("END SENTENCES")
+
     return all_words_amount(text) / len(sentences)
+
+
+def get_median(numbers_list):
+    size = len(numbers_list)
+
+    if size % 2 == 0:
+        median = (numbers_list[int(size / 2)] + numbers_list[int(size / 2) - 1]) / 2
+    else:
+        median = numbers_list[int(size / 2)]
+
+    return median
+
+
+def median_words_in_sentence(text):
+    sentences = split_into_sentences(text)
+    words_amounts = list()
+
+    for curr_sentence in sentences:
+        words_amounts.append(all_words_amount(curr_sentence))
+
+    words_amounts.sort()
+
+    return get_median(words_amounts)
 
 
 def main():
     inp_text = input("enter some text please: ")
-    
+
     print("\ncount every word: ")
     words = count_every_word(inp_text)
 
@@ -54,6 +78,8 @@ def main():
         print(w + ": " + str(words[w]) + " times")
 
     print("\naverage words in a sentence: " + str(average_words_in_sentence(inp_text)))
+
+    print("\nmedian words in a sentence: " + str(median_words_in_sentence(inp_text)))
 
 
 if __name__ == "__main__":
