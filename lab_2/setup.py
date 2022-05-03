4d17348ed3016cbed0f5cd05e4cbea744cc7aa6a
aneroid11 <a-e-v2003@yandex.by>
2022-05-03 23:22:25 +0300

    fixed setup.py, package is installed successfully

--- lab_2/setup.py
+++ lab_2/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='pyobjserializer',
-    version='1.6',
+    version='1.7',
     description='A library to serialize Python objects and a console tool to convert object from one '
                 'format to another',
     author='aneroid11',
     packages=['pyobjserializer'],
-    install_requires=['tomli_w', 'tomli', 'pyyaml', 'coverage'],
-    scripts=['pyobjserializer/pyobjconverter.py']
+    install_requires=['tomli_w', 'tomli', 'pyyaml'],
+    scripts=['pyobjconverter.py']
 )
