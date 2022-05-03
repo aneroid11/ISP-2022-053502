d47a620ce13cae072c22556714bb989467e2cec2
aneroid11 <a-e-v2003@yandex.by>
2022-05-03 19:28:51 +0300

    converting files successfully

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,15 +1,36 @@
-import sys
+from sys import exit
 from argparse import ArgumentParser
 from configparser import ConfigParser
 from create_serializer import create_serializer
 
 
+def convert_file(input_file_name: str, initial_format: str, output_format: str):
+    if initial_format == output_format:
+        return None
+
+    output_file_name = input_file_name + "." + output_format
+    input_file_name += "." + initial_format
+
+    try:
+        input_serializer = create_serializer(initial_format)
+        output_serializer = create_serializer(output_format)
+    except NotImplementedError:
+        print("no serializer for", initial_format, "or", output_format)
+        exit(1)
+
+    with open(input_file_name, "r") as file:
+        obj = input_serializer.load(file)
+
+    with open(output_file_name, "w") as file:
+        output_serializer.dump(obj, file)
+
+
 def main():
     parser = ArgumentParser()
-    parser.add_argument("-i", "--input", type=str, help="input file")
-    parser.add_argument("-f", "--format", type=str, help="format to convert the input file to")
-    parser.add_argument("-o", "--output", type=str, help="output file")
-    parser.add_argument("-c", "--config-file", type=str, help="config file in json format")
+    parser.add_argument("-i", "--input", help="input file (without the extension)")
+    parser.add_argument("--initial-format", help="input file format")
+    parser.add_argument("--output-format", help="format to convert the input file to")
+    parser.add_argument("-c", "--config-file", help="config file in json format")
     args = parser.parse_args()
 
     if args.config_file is not None:
@@ -18,26 +39,26 @@ def main():
         config.read(conf_file_name)
 
         input_file_name = config["info"]["input"]
-        output_file_name = config["info"]["output"]
-        format_name = config["info"]["format"]
+        initial_format_name = config["info"]["initial_format"]
+        output_format_name = config["info"]["output_format"]
     else:
         if args.input is None:
             print("you must specify the input file name")
-            sys.exit(1)
-        if args.output is None:
-            print("you must specify the output file name")
-            sys.exit(1)
-        if args.format is None:
-            print("you must specify the format name")
-            sys.exit(1)
+            exit(1)
+        if args.initial_format is None:
+            print("you must specify the initial format")
+            exit(1)
+        if args.output_format is None:
+            print("you must specify the output file format")
+            exit(1)
 
         input_file_name = args.input
-        output_file_name = args.output
-        format_name = args.format
+        initial_format_name = args.initial_format
+        output_format_name = args.output_format
 
-    print(input_file_name)
-    print(output_file_name)
-    print(format_name)
+    convert_file(input_file_name, initial_format_name, output_format_name)
+    print("converted", input_file_name + "." + initial_format_name,
+          "to", input_file_name + "." + output_format_name)
 
 
 if __name__ == '__main__':
