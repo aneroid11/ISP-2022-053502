ac789c5e6580d1af0d9cbc1016c3100d20eec41f
aneroid11 <a-e-v2003@yandex.by>
2022-05-03 14:05:51 +0300

    check coverage

---
+++ lab_2/tests.py
@@ -0,0 +1,11 @@
+import unittest
+from create_serializer import create_serializer
+
+
+class TestSomething(unittest.TestCase):
+    def test_true_is_true(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == "__main__":
+    unittest.main()
