aa6a575baeda6fa9b03c3b26f14c7745b6243a06
aneroid11 <a-e-v2003@yandex.by>
2022-05-04 11:27:19 +0300

    start documenting

--- lab_2/loading_tests.py
+++ lab_2/loading_tests.py
@@ -1,9 +1,12 @@
+"""This module is needed for loading objects in an environment that differs from which they were dumped from."""
+
 from pyobjserializer.create_serializer import create_serializer
 
 c = 553
 
 
 def load_object(serializer_name: str) -> object:
+    """Load an object from test_serialized_object.[format_name] file."""
     serializer = create_serializer(serializer_name)
 
     with open("test_serialized_object." + serializer_name, "r") as file:
--- lab_2/run_linters_and_formatters.sh
+++ lab_2/run_linters_and_formatters.sh
@@ -2,3 +2,4 @@ black *.py pyobjserializer/*.py
 isort *.py pyobjserializer/*.py
 mypy *.py pyobjserializer/*.py
 flake8 --max-line-length 120 *.py pyobjserializer/*.py
+pydocstyle *.py ./pyobjserializer/*.py
