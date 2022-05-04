b1550cb4fad833e389ad265f703362094f1df38b
aneroid11 <a-e-v2003@yandex.by>
2022-05-04 17:53:10 +0300

    docstrings for abstract_serializer

--- lab_2/pyobjserializer/abstract_serializer.py
+++ lab_2/pyobjserializer/abstract_serializer.py
@@ -1,3 +1,5 @@
+"""Module that defines the AbstractSerializer class and some functions to implement it."""
+
 import types
 from typing import TextIO
 
@@ -5,23 +7,30 @@ from . import converter
 
 
 class AbstractSerializer:
+    """Abstract serializer that can dump/load from/to a file and a string."""
+
     def dumps(self, obj: object) -> str:
+        """Serialize obj to a string."""
         raise NotImplementedError()
 
     def dump(self, obj: object, fp: TextIO):
+        """Serialize obj to the fp stream."""
         # fp is a file descriptor, not file name
         text_to_write = self.dumps(obj)
         fp.write(text_to_write)
 
     def loads(self, string: str, globs: dict = None) -> object:
+        """Deserialize obj from string. Optionally update object globals using globs."""
         raise NotImplementedError()
 
     def load(self, fp: TextIO, globs: dict = None) -> object:
+        """Deserialize obj from the fp stream. Optionally update object globals using globs."""
         data_string = str(fp.read())
         return self.loads(data_string, globs)
 
 
 def dumps_using_dumps_elementary(obj: object, dumps_elementary) -> str:
+    """Dump a complex object using a function to dump an elementary object."""
     if converter.object_of_elementary_type(obj):
         dumped = dumps_elementary(obj)
     else:
@@ -41,6 +50,7 @@ def dumps_using_dumps_elementary(obj: object, dumps_elementary) -> str:
 def loads_using_loads_elementary(
     string: str, loads_elementary, globs: dict = None
 ) -> object:
+    """Load a complex object using a function to load an elementary object."""
     decoded_object = loads_elementary(string)
     globs_passed = None if globs is None else globs.copy()
 
